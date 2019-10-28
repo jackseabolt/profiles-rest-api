4532a92035a937460bcb81ec607b9e2af0f6e80f
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2019-10-28 07:03:02 -0700

    django admin setup

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -19,13 +19,13 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_superUser(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(user=self._db)
+        user.save(using=self._db)
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
